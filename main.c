e1ea900e5243ff21b6c04735277fb934403d3ba2
Temo2021 <zhangfei094@outlook.com>
2026-06-11 15:28:36 +0800

    test

--- main.c
+++ main.c
@@ -160,7 +160,7 @@ int main(void)
     while (1)
     {
         // LED иак╦
-        if(HAL_GetTick() - led_tick >= 500)
+        if(HAL_GetTick() - led_tick >= 300)
         {
             HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_4);
             HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
